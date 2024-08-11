bf619c531524c243bf26f98213606ccf6402e9c8
Areful Islam Asif <arefulislam015@gmail.com>
2024-08-11 13:43:28 +0600

    if and switch statement

--- vowel_or_consonent.c
+++ vowel_or_consonent.c
@@ -4,7 +4,7 @@ int main(){
     printf("Enter a charatcher: ");
     scanf("%c", &userCha);
     // use of if else statement and logical operator
-    
+/*
     if ( userCha=='a'|| userCha== 'e'|| userCha=='i' || userCha=='o'|| userCha=='u'|| userCha=='A'|| userCha=='E'|| userCha=='I' || userCha=='O'||userCha=='U'){
     
         printf("'%C' is Vowel", userCha);
@@ -17,7 +17,58 @@ int main(){
     else{
         printf("'%C' is Consonent ", userCha);
     }
+*/
 
+// ues of switch statement
+switch (userCha)
+{
+case 'a':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'A':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'e':
+    printf("'%C' is Vowel", userCha);
+    break;
+
+case 'E':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'i':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'I':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'o':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'O':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'u':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'U':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'w':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'W':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'y':
+    printf("'%C' is Vowel", userCha);
+    break;
+case 'Y':
+    printf("'%C' is Vowel", userCha);
+    break;
+default:
+ printf("'%C' is Consonent ", userCha);
+    break;
+}
 
     return 0;
 }
